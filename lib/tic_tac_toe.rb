ef67f344094b5cb48323a7a4879230a9b4900c67
Kimberly Fritter <littlekimmy44@hotmail.com>
2019-03-04 17:32:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -90,7 +90,7 @@ def play(board)
   if won?(board)
     puts "Congratulations #{winner(board)}"
   elsif draw?(board)
-    puts " Game is a draw"
+    puts "Cat's Game!"
   else
     turn(board)
     turn += 1 
